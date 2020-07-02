c17e5e6326619689b328dd2229f406c1771a93bc
liaozijian <lzj960515@163.com>
2020-07-02 17:37:17 +0800

    JMM

--- my-juc-test/src/com/my/juc/test/volatiletest/VisualizationTest.java
+++ my-juc-test/src/com/my/juc/test/volatiletest/VisualizationTest.java
@@ -1,6 +1,7 @@
 package com.my.juc.test.volatiletest;
 
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.LockSupport;
 
 /**
  * 测试 volatile可视化
@@ -15,7 +16,12 @@ public class VisualizationTest {
         new Thread(()->{
             System.out.println(name + " 启动中......");
             while (flag){
-
+                try {
+                    TimeUnit.SECONDS.sleep(1);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println("当前静态变量 flag:"+flag);
             }
             System.out.println(name + " 执行完毕......");
         },name).start();
@@ -27,5 +33,8 @@ public class VisualizationTest {
             flag = false;
             System.out.println(name2 + " 修改静态变量flag:"+flag);
         }, name2).start();
+       Thread t = new Thread();
+       LockSupport.parkUntil();
+       LockSupport.parkNanos();
     }
 }
